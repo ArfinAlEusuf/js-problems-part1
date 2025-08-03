3dc98472ad7256db278b007d2c92a537830e7497
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-08-03 20:48:16 +0600

    leap year

---
+++ leap-year.js
@@ -0,0 +1,18 @@
+/**
+ * year will be a leap year if the year is divisible by 4
+ */
+
+function isleapYear(year){
+    if( year % 400 === 0){
+        return true;
+    }
+    else if( year % 4 ===0 && year % 100 !== 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const isLeap = isleapYear(2000);
+console.log(isLeap);
