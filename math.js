b7a189e7c3429f29b55b58b7cb6350d15c0bad8d
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-08-04 02:02:26 +0600

    rounded and abs

--- math.js
+++ math.js
@@ -11,3 +11,7 @@ console.log(min);
 const max = Math.max(45, 21, 65, 99, 126);
 console.log(max);
 
+console.log(Math.PI);
+console.log(Math.abs(10-5));
+console.log(Math.round(2.25));
+console.log(Math.round(2.85));
