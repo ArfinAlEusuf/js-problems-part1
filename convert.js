adf99ced9d47b941cd16b0f7addf25dfe542e742
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-08-03 20:27:22 +0600

    hight conversion

--- convert.js
+++ convert.js
@@ -5,5 +5,15 @@ function inchToFeet(inch){
     return feet;
 }
 
+// 75
+function inchToFeet2(inch){
+    const feetFraction = inch/12;
+    const feetNumber = parseInt(feetFraction);
+    const num = feetNumber * 12
+    const inchNumber = inch - num;
+    console.log(feetNumber, inchNumber)
+}
+
 const shovoHeight = inchToFeet(75);
-console.log(shovoHeight);
+// console.log(shovoHeight);
+const shovoHeight2 = inchToFeet2(75);
