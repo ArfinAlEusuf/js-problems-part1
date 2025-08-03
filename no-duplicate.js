d5ebb7a7cda6258fddda4461481f10f20074229c
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-08-04 01:40:08 +0600

    unique array

---
+++ no-duplicate.js
@@ -0,0 +1,20 @@
+/**
+ * array has some duplicate elements
+ * []
+ */
+
+
+const biriyaniKhor = ['abul', 'babul', 'cabul', 'abul', 'babul', 'dabul', 'kabul', 'cabul'];
+
+function noDuplicate(array){
+    const unique = [];
+    for(const item of array){
+        if(unique.includes(item)=== false){
+            unique.push(item);
+        }
+    }
+    return unique;
+}
+
+const uniqueArry = noDuplicate(biriyaniKhor);
+console.log(uniqueArry)
